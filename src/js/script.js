0aaab01acc57c303fd413fc3c50b1247d785d2b1
Patryk <patrykchmielewskipl@gmail.com>
2023-03-26 01:23:18 +0100

    add upcoming races section from 1200px and fix cards on all breakpoints

---
+++ src/js/script.js
@@ -0,0 +1,40 @@
+const burgerBtn = document.querySelector('.burger-btn')
+const mobileNav = document.querySelector('.mobile__nav')
+
+const accordions = document.querySelectorAll(".accordion");
+
+const showMenu = () => {
+	mobileNav.classList.toggle('mobile-nav-active')
+}
+
+
+
+
+const openAccordion = (accordion) => {
+	const content = accordion.querySelector(".accordion__content");
+	accordion.classList.add("accordion__active");
+	content.style.maxHeight = content.scrollHeight + "px";
+};
+
+const closeAccordion = (accordion) => {
+	const content = accordion.querySelector(".accordion__content");
+	accordion.classList.remove("accordion__active");
+	content.style.maxHeight = null;
+};
+
+accordions.forEach((accordion) => {
+	const intro = accordion.querySelector(".accordion__intro");
+	const content = accordion.querySelector(".accordion__content");
+
+	intro.onclick = () => {
+		if (content.style.maxHeight) {
+			closeAccordion(accordion);
+		} else {
+			accordions.forEach((accordion) => closeAccordion(accordion));
+			openAccordion(accordion);
+		}
+	};
+});
+
+
+burgerBtn.addEventListener('click', showMenu)
