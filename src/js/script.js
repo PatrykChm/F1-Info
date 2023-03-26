6b498a7e52ec6794d05ee70fd97cd797f5f2f54e
Patryk <patrykchmielewskipl@gmail.com>
2023-03-26 21:39:04 +0200

    add flags into accorions and JS concept to hide flags after open accordion

--- src/js/script.js
+++ src/js/script.js
@@ -1,40 +1,50 @@
 const burgerBtn = document.querySelector('.burger-btn')
 const mobileNav = document.querySelector('.mobile__nav')
-
-const accordions = document.querySelectorAll(".accordion");
+const accordions = document.querySelectorAll('.accordion')
+const accordionsIntro = document.querySelectorAll('.accordion__intro')
+const raceFlag = document.querySelector('.raceflag')
 
 const showMenu = () => {
 	mobileNav.classList.toggle('mobile-nav-active')
 }
 
+const openAccordion = accordion => {
+	const content = accordion.querySelector('.accordion__content')
+	accordion.classList.add('accordion__active')
+	content.style.maxHeight = content.scrollHeight + 'px'
+}
 
+const closeAccordion = accordion => {
+	const content = accordion.querySelector('.accordion__content')
+	accordion.classList.remove('accordion__active')
+	content.style.maxHeight = null
+}
 
-
-const openAccordion = (accordion) => {
-	const content = accordion.querySelector(".accordion__content");
-	accordion.classList.add("accordion__active");
-	content.style.maxHeight = content.scrollHeight + "px";
-};
-
-const closeAccordion = (accordion) => {
-	const content = accordion.querySelector(".accordion__content");
-	accordion.classList.remove("accordion__active");
-	content.style.maxHeight = null;
-};
-
-accordions.forEach((accordion) => {
-	const intro = accordion.querySelector(".accordion__intro");
-	const content = accordion.querySelector(".accordion__content");
+accordions.forEach(accordion => {
+	const intro = accordion.querySelector('.accordion__intro')
+	const content = accordion.querySelector('.accordion__content')
 
 	intro.onclick = () => {
 		if (content.style.maxHeight) {
-			closeAccordion(accordion);
+			closeAccordion(accordion)
 		} else {
-			accordions.forEach((accordion) => closeAccordion(accordion));
-			openAccordion(accordion);
+			accordions.forEach(accordion => closeAccordion(accordion))
+			openAccordion(accordion)
 		}
-	};
-});
+	}
+})
 
+const chceckFlag = () => {
+	accordions.forEach(accordion => {
+		accordion.addEventListener('click', function () {
+			if (accordion.classList.contains('accordion__active')) {
+				raceFlag.style.display = 'none'
+			} else {
+				raceFlag.style.display = 'block'
+			}
+		})
+	})
+}
 
+chceckFlag()
 burgerBtn.addEventListener('click', showMenu)
